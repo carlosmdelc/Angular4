c9183065a722de96bd124b24f345753bdb5041b6
carlosmdelc <carlosmdelc@gmail.com>
2018-03-06 18:14:28 -0600

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
